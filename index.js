60d32d8f7974ec0c92d56d29b67660a9cf82e37c
aartVL <aartvanleunen08@gmail.com>
2024-12-18 11:32:17 +0100

    m

---
+++ index.js
@@ -0,0 +1,4 @@
+// Pop-up melding als je op de pagina komt
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Welkom bij het redden van kerst bordspel! Lees de regels en bereid je voor.");
+});
